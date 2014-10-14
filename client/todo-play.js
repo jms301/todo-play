6b7f53816b63376a69a4d13d827c2de392934ea2
James Southren <jms301@gmail.com>
2014-10-14 10:22:21 +0200

    hold ga tracking code till document has loaded with jquery

--- client/todo-play.js
+++ client/todo-play.js
@@ -133,15 +133,18 @@ Tracker.autorun( function (comp) {
 });
 
 Meteor.startup( function () {
+$(document).ready(function() {
   if (document.location.hostname.search("callsar.com") !== -1) {
+    console.log("GA tracking is happening");
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-55690981-1', 'auto');
-    ga('send', 'pageview');
- }
+      ga('create', 'UA-55690981-1', 'callsar.com');
+      ga('send', 'pageview');
+    }
+  });
 });
 
 // update the day every min
