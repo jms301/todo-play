ac9691a3b79e4f422667d8b1ef72363904b9dfc1
James Southern <jms301@gmail.com>
2014-10-30 18:17:09 +0000

    remove defunct package

--- packages/generictodoitem/setup.js
+++
@@ -1,8 +0,0 @@
-UserConfig = new Meteor.Collection("user_config");
-
-Router.route('/config', {
-  data: function () {
-    return (UserConfig.findOne({userId: Meteor.userId()}) || DefaultUserConfig);
-  }
-});
-
