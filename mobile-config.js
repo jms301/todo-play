e571adc14ed525513e5c2ec2ec4f6fc54b37a495
James Southern <jms301@gmail.com>
2015-01-28 17:37:41 +0000

    mobile config for android store

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Todo Play',
+  description: 'A simple GTD style todo list app',
+  version: '0.0.2'
+  });
+
+/*App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});*/
