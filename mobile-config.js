dacf04ce2796905688b9ae133315821b491921fb
James Southern <jms301@gmail.com>
2015-03-30 19:45:26 +0100

    add in icons and splash

--- mobile-config.js
+++ mobile-config.js
@@ -4,9 +4,38 @@ App.info({
   version: '0.0.21'
   });
 
-/*App.icons({
-  'android_ldpi': 'resources/icons/icon-ldpi.png',
-  'android_mdpi': 'resources/icons/icon-mdpi.png',
-  'android_hdpi': 'resources/icons/icon-hdpi.png',
-  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
-});*/
+App.icons({
+  // iOS
+  //'iphone': 'resources/icons/icon-60x60.png',
+  //'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+  //'ipad': 'resources/icons/icon-72x72.png',
+  //'ipad_2x': 'resources/icons/icon-72x72@2x.png',
+
+  // Android
+  'android_ldpi': 'resources/icons/icon.36.png',
+  'android_mdpi': 'resources/icons/icon.48.png',
+  'android_hdpi': 'resources/icons/icon.72.png',
+  'android_xhdpi': 'resources/icons/icon.96.png'
+});
+
+App.launchScreens({
+  // iOS
+  //'iphone': 'resources/splash/splash-320x480.png',
+  //'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+  //'iphone5': 'resources/splash/splash-320x568@2x.png',
+  //'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  //'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  //'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  //'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+
+  // Android
+  'android_ldpi_portrait': 'resources/splash/splash.200x320.png',
+  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash.320x480.png',
+  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash.480x800.png',
+  //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash.720x1280.png',
+  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+});
+
